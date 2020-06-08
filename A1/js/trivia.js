abe4e73fb82240000b1a83416e62d5dd9fb3c8ff
bmatiascxv <matiaben@sheridancollege.ca>
2020-06-08 03:32:01 -0400

    Added simple css

--- A1/js/trivia.js
+++ A1/js/trivia.js
@@ -1,45 +1,42 @@
 var questionCounter = 0;
-var scoreCounter = 0;
+var scoreCounter = 1;
 
 // Function called nextQuestion that goes up increments when the Next Question button is clicked //
 function nextQuestion(){
   if(questionCounter === 0){
     document.getElementById("Question").innerHTML = "<h3> Question 2 | Who is the writer of the famous Dragon Ball series </h3>";
-    document.getElementById("optionA").innerHTML = "Akira Toriyama";
+    document.getElementById("optionA").innerHTML = "Akira Toriyama",  scoreCounter++;
     document.getElementById("optionB").innerHTML = "Osamu Tezuka" ;
     document.getElementById("optionC").innerHTML = "Go Nagai";
     document.getElementById("optionD").innerHTML = "Eiichiro Oda" ;
     questionCounter++;
-    clearAnswers();
   }
   else if(questionCounter === 1){
     document.getElementById("Question").innerHTML = "<h3> Question 3 | What anime is known for the Acronym FMA </h3>";
     document.getElementById("optionA").innerHTML = "Fin's Mad Adventure";
-    document.getElementById("optionB").innerHTML = "Full Metal Alchemist";
+    document.getElementById("optionB").innerHTML = "Full Metal Alchemist",  scoreCounter++;
     document.getElementById("optionC").innerHTML = "Full Metal Adventure";
     document.getElementById("optionD").innerHTML = "Fairy Memory Advent";
     questionCounter++;
-    clearAnswers();
   }
   else if(questionCounter === 2){
     document.getElementById("Question").innerHTML = "<h3> Question 4 | What was Ash's starter pokemon </h3>";
     document.getElementById("optionA").innerHTML = "Turtwig";
     document.getElementById("optionB").innerHTML = "Charmander";
     document.getElementById("optionC").innerHTML = "Piplup";
-    document.getElementById("optionD").innerHTML = "Pikachu";
+    document.getElementById("optionD").innerHTML = "Pikachu",  scoreCounter++;
     questionCounter++;
-    clearAnswers();
-    
   }
+  // User Clicks play again then page reloads
   else if(questionCounter === 3){
     document.getElementById("Question").innerHTML = "<h2>Score: " + scoreCounter + "</h2>";
-    document.getElementById("submit").innerHTML = "Play Again";
+    document.getElementById("submit").innerHTML = "Play Again"; 
     questionCounter++;
     document.getElementById("choiceTable").style.visibility = "hidden";
-    clearAnswers();
   }    
   else if(questionCounter === 4){
-      location.reload();
+      location.reload(); // Reloads the page
   }
-  
 }
+
+
